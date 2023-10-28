bb5c3b631c9bb1ef109382ed4df506b6822cefc1
Alejandro Ortega <ort18248@uvg.edu.gt>
2023-10-28 16:57:58 -0600

    Updated: switched to a 'CYKParser' class for the parser
    Also modified the example usage accordingly

--- src/cyk_test.py
+++ src/cyk_test.py
@@ -1,51 +1,49 @@
-def cyk_parse(grammar, sentence):
-    variables = grammar["VARIABLES"]
-    n = len(sentence)
+import json
+from typing import Dict
 
-    # Initialize the CYK table with empty sets
-    table = [[set() for _ in range(n)] for _ in range(n)]
+class CYKParser:
+    def __init__(self, cnf_grammar: Dict[str, any]):
+        self.grammar = cnf_grammar
 
-    # Fill in the diagonal with the terminals that match the words in the sentence
-    for i in range(n):
-        for variable in variables:
-            if sentence[i] in grammar["REGLAS"].get(variable, []):
-                table[i][i].add(variable)
+    def parse(self, sentence: str) -> bool:
+        variables = self.grammar["VARIABLES"]
+        n = len(sentence.split())
 
-    # Fill in the rest of the table
-    for length in range(2, n + 1):
-        for i in range(n - length + 1):
-            j = i + length - 1
-            for k in range(i, j):
-                for variable in variables:
-                    for rule in grammar["REGLAS"].get(variable, []):
-                        if len(rule.split()) == 2:
-                            left, right = rule.split()
-                            if left in table[i][k] and right in table[k + 1][j]:
-                                table[i][j].add(variable)
+        # Initialize the CYK table with empty sets
+        table = [[set() for _ in range(n)] for _ in range(n)]
 
-    # If the start symbol is in the top-right cell, the string is in the language
-    return grammar["INICIAL"] in table[0][n - 1]
+        # Fill in the diagonal with the terminals that match the words in the sentence
+        for i, word in enumerate(sentence.split()):
+            for variable in variables:
+                if word in self.grammar["REGLAS"].get(variable, []):
+                    table[i][i].add(variable)
+
+        # Fill in the rest of the table
+        for length in range(2, n + 1):
+            for i in range(n - length + 1):
+                j = i + length - 1
+                for k in range(i, j):
+                    for variable in variables:
+                        for rule in self.grammar["REGLAS"].get(variable, []):
+                            if len(rule.split()) == 2:
+                                left, right = rule.split()
+                                if left in table[i][k] and right in table[k + 1][j]:
+                                    table[i][j].add(variable)
+
+        # If the start symbol is in the top-right cell, the sentence is in the language
+        return self.grammar["INICIAL"] in table[0][n - 1]
 
 # Example usage
 if __name__ == "__main__":
-    grammar = {
-        "INICIAL": "S",
-        "VARIABLES": ["S", "VP", "PP", "NP", "V", "P", "N", "Det"],
-        "TERMINALES": ["cooks", "drinks", "eats", "cuts", "he", "she", "in", "with", "cat", "dog", "beer", "cake", "juice", "meat", "soup", "fork", "knife", "oven", "spoon", "a", "the"],
-        "REGLAS": {
-            "S": ["NP VP"],
-            "VP": ["VP PP", "V NP", "cooks", "drinks", "eats", "cuts"],
-            "PP": ["P NP"],
-            "NP": ["Det N", "he", "she"],
-            "V": ["cooks", "drinks", "eats", "cuts"],
-            "P": ["in", "with"],
-            "N": ["cat", "dog", "beer", "cake", "juice", "meat", "soup", "fork", "knife", "oven", "spoon"],
-            "Det": ["a", "the"]
-        }
-    }
-
+    # Specify the correct file path
+    grammar_file_path = "src/input.json"
+    
+    with open(grammar_file_path, "r") as grammar_file:
+        grammar_json = json.load(grammar_file)
+    
+    cyk_parser = CYKParser(grammar_json)
     sentence = "he cooks with a knife"
-    result = cyk_parse(grammar, sentence.split())
+    result = cyk_parser.parse(sentence)
     
     print(f"Accepted: {result}")
     if result:
